781a40020e32db3682a33a1d3406b8578c58290b
Austomus Gemmail <austomus.minecraft@gmail.com>
2021-09-10 14:26:06 +1200

    use error function and add feature to count tries to get to number

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-# randGenUntil
-python program that generates random numbers until the number generated is the same as the number specified
+# randgenuntil
+Python program that generates random numbers until the number generated is the same as the number specified.
 
-please tell me if anything is wrong with my code, or if you think the name is bad
+## Help
+
+```
+usage: randgenuntil.py [-h] [-s START] [-e END] [-c] [-n] number
+
+positional arguments:
+  number                Generate a random numbers until they are equal to this.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -s START, --start START
+                        The range in which the random numbers are in starts with this number. (default 0)
+  -e END, --end END     The range in which the random numbers are in ends with this number. (default 32767)
+  -c, --count           Counts the amount of tries it takes to get to the number.
+  -n, --newline         Adds a newline between random numbers.
+  ```
+
+### Contributing
+
+Please tell me if you think anything is wrong with it by creating an issue or if you fixed it, a pull request.
+If you created a new feature you can also create a pull request.
+
--- randgenuntil.py
+++ randgenuntil.py
@@ -3,34 +3,40 @@ import random
 import argparse
 import sys
 
-parser = argparse.ArgumentParser("randGenUntil")
-parser.add_argument("number", help="Generate a random numbers until they are equal to this.", type=int)
+
+def error(message):
+    print(message)
+    sys.exit(1)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("number",
+                    help="Generate a random numbers until they are equal to this.", type=int)
 parser.add_argument("-s", "--start", type=int, default=0,
                     help="The range in which the random numbers are in starts with this number. (default 0)")
 parser.add_argument("-e", "--end", type=int, default=32767,
                     help="The range in which the random numbers are in ends with this number. (default 32767)")
-parser.add_argument("-n", "--newline", help="Adds a newline between random numbers.", action="store_true")
-
-
-def error(message, exit_code):
-    print(message)
-    sys.exit(exit_code)
-
+parser.add_argument("-c", "--count",
+                    help="Counts the amount of tries it takes to get to the number.", action="store_true")
+parser.add_argument("-n", "--newline",
+                    help="Adds a newline between random numbers.", action="store_true")
 
 args = parser.parse_args()
 if args.start > args.end:
-    print("error: start is greater than end")
-    sys.exit(1)
+    error("error: start is greater than end")
 if args.number > args.end or args.number < args.start:
-    print("error: number is either greater than end or less than start")
-    sys.exit(1)
-end = ''
+    error("error: number is either greater than end or less than start")
 end = "\n" if args.newline else "\r"
 rand_num = ''
+tries = 0
 args.end += 1
 while rand_num != args.number:
     width = len(str(rand_num))
     rand_num = random.randrange(args.start, args.end)
     print("{rand_num: <{width}}".format(rand_num=rand_num, width=width), end=end)
-if end == "\r":
+    tries += 1
+if args.count:
+    print("{} tries to get to {}".format(tries, args.number))
+elif end == "\r":
     print()
+
